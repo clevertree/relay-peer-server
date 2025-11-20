9634c05f7bb8c5da946823de46a903aaba68cb01
Ari Asulin <Ari.Asulin@gmail.com>
2025-11-20 17:08:02 -0500

    compile fixes

--- models/Peer.ts
+++ models/Peer.ts
@@ -1,33 +1,22 @@
-import {
-  Table,
-  Column,
-  Model,
-  DataType,
-  Index,
-  PrimaryKey,
-  AutoIncrement,
-  Unique,
-  UpdatedAt,
-  CreatedAt,
-} from 'sequelize-typescript';
+import {Column, DataType, Index, Model, Table, Unique,} from 'sequelize-typescript';
 
-@Table({ tableName: 'peer', timestamps: true })
-export class Peer extends Model<Peer> {
-  @PrimaryKey
-  @AutoIncrement
-  @Column(DataType.INTEGER)
-  id!: number;
+@Table({tableName: 'peer', timestamps: true})
+export class Peer extends Model {
+    // @PrimaryKey
+    // @AutoIncrement
+    // @Column(DataType.INTEGER)
+    // id!: number;
 
-  @Unique
-  @Index
-  @Column({ type: DataType.STRING, allowNull: false })
-  socket!: string;
+    @Unique
+    @Index
+    @Column({type: DataType.STRING, allowNull: false})
+    socket!: string;
 
-  @CreatedAt
-  @Column(DataType.DATE)
-  createdAt!: Date;
-
-  @UpdatedAt
-  @Column(DataType.DATE)
-  updatedAt!: Date;
+    // @CreatedAt
+    // @Column(DataType.DATE)
+    // createdAt!: Date;
+    //
+    // @UpdatedAt
+    // @Column(DataType.DATE)
+    // updatedAt!: Date;
 }
